b2e3fb4d261e71e7c6f66deeb629952f7676e4ed
Juan Manuel De Senzi <juanmadesenzi@gmail.com>
2021-04-25 18:52:56 -0300

    falta pocco para desafío 3

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -9,46 +9,49 @@ import HippieImg from '../../multimedia/Hippie.jpg';
 export default function ItemListContainer () {
     const [productos, setProductos] = useState([]);
 
+    console.log('Hola me rendericé')
+    const catalogo = [
+        {
+            id: 1,
+            titulo: 'Walden',
+            autor: 'Henry Thoreau',
+            imagen: {WaldenImg}
+        },
+        {
+            id: 2,
+            titulo: 'Hacia un futuro vegano',
+            autor: 'Tobias Leenaert',
+            imagen: {HaciaUnFuturoVeganoImg}
+        },
+        {
+            id: 3,
+            titulo: 'Un mundo feliz',
+            autor: 'Aldous Huxley',
+            imagen: {UnMundoFelizImg}
+        },
+        {
+            id: 4,
+            titulo: 'Hippie',
+            autor: 'Paulo Coelho',
+            imagen: {HippieImg}
+        }
+    ];
+
     useEffect(()=>{
         const task = new Promise((resolve, reject) => {
-        const catalogo = [
-                {
-                    id: 1,
-                    titulo: 'Walden',
-                    autor: 'Henry Thoreau',
-                    imagen: {WaldenImg}
-                },
-                {
-                    id: 2,
-                    titulo: 'Hacia un futuro vegano',
-                    autor: 'Tobias Leenaert',
-                    imagen: {HaciaUnFuturoVeganoImg}
-                },
-                {
-                    id: 3,
-                    titulo: 'Un mundo feliz',
-                    autor: 'Aldous Huxley',
-                    imagen: {UnMundoFelizImg}
-                },
-                {
-                    id: 4,
-                    titulo: 'Hippie',
-                    autor: 'Paulo Coelho',
-                    imagen: {HippieImg}
-                }
-            ];
             setTimeout(()=>{
                 resolve(catalogo)
             }, 3000)
         });
 
 
-    task.then(resolved => {
-        setProductos(resolved)
-    });
-
+        task.then(resolved => {
+            setProductos(resolved)
+        });
     }, [])
     
+    
+
     return (
         <div className='ItemListContainer'>
             {productos.length>0 ? <ItemList props={productos} /> : <h1>Cargando catálogo...</h1>}
